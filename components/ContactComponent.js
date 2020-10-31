1e37eae423d7722e9b1ef0fac08bad1fe7f5f75a
Patience Dendy <dendypat@gmail.com>
2020-10-31 09:46:15 -0400

    Gestures

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native'
 import { Card } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -9,20 +10,21 @@ class Contact extends Component {
     super(props);
     this.state = {
     };
-}
+  }
   static navigationOptions = {
     title: ' Contact Us '
-}
+  }
   render() {
     return (
       <ScrollView>
-        <Card title="Contact Information" wrapperStyle={{margin: 20}}>
-
-          <Text>1 Nucamp Way</Text>
-          <Text>Seattle, WA 98001</Text>
-          <Text style={{marginBottom: 10}}>U.S.A</Text>
-          <Text>Phone: 1-206-555-1234</Text>
-          <Text>Email: campsites@nucamp.co</Text>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+          </Animatable.View>
         </Card>
       </ScrollView>
     )
